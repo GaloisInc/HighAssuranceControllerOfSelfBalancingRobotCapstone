7622e48f1ee9bb4a9bee9ba5abc85d43319f55bf
artem1199 <artkulak@gmail.com>
2020-04-06 04:29:39 -0700

    experimenting with rust FFI

---
+++ kind2/lus/pid.lus.out/compute_pid/implem/target/debug/build/compute_pid_implem-dfb753a706fd8511/build_script_build-dfb753a706fd8511.d
@@ -0,0 +1,5 @@
+/home/artem/Desktop/GitRepos/Galois-ECE/kind2/lus/pid.lus.out/compute_pid/implem/target/debug/build/compute_pid_implem-dfb753a706fd8511/build_script_build-dfb753a706fd8511: rsc/build.rs
+
+/home/artem/Desktop/GitRepos/Galois-ECE/kind2/lus/pid.lus.out/compute_pid/implem/target/debug/build/compute_pid_implem-dfb753a706fd8511/build_script_build-dfb753a706fd8511.d: rsc/build.rs
+
+rsc/build.rs:
